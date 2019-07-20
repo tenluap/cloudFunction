52d57c3437278f99ce8d01f87283899fdd538ab8
ehigiepaul <ehigiepaul@yahoo.com>
2019-07-20 04:35:16 +0100

    versio 1.2

--- index.ts
+++ index.ts
@@ -1,15 +1,8 @@
 import { Request, Response } from 'express'
 
-export let main = (req: Request, res: Response) => {
+export let main = (req?: Request, res?: Response) => {
     // code here
-    let r = [{ name: 'ehigiepaul' }]
-    Array.from(r).forEach((rs) => {
-        rs
-    })
-
-    res.send(r[0])
-
-
 
 }
+
 
